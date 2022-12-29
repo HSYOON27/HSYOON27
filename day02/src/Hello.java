baee504552358bfefa66688834b679bed201c52d
HSYOON27 <kots524@naver.com>
2022-12-29 11:04:27 +0900

    설명추가

--- day02/src/Hello.java
+++ day02/src/Hello.java
@@ -1,6 +1,6 @@
-import java.lang.*;
-public class Hello {
-      public static void main(String[] args) {
+import java.lang.*;//프로그램에 필요한 도구를 준비하는 것. JRE System Library에서 가져온다는거. 도구를 사실 없어도 되는데 (만들어 줌 자동으로. 하지만 인식을 위해)
+public class Hello {//파일을 대표하는 영역
+      public static void main(String[] args) {//프로그램 시작 영역
     	  System.out.println("Hello java!");
     	  System.out.println("Hello java!");
     	  //System.out.println("Hello java!");
