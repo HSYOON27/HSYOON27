0200dbb9527352d4f9491cf7cedb0a428fa10400
HSYOON27 <kots524@naver.com>
2023-03-07 16:49:03 +0900

    세미 프로젝트 설정

---
+++ semiproject1/src/test/java/com/kh/semi/Semiproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.semi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semiproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
