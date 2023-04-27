bcb845b76850cb06b0eda2958b075ccd2e8e1d68
HSYOON27 <kots524@naver.com>
2023-04-27 14:04:53 +0900

    회원가입 및 로그인 암호화 모듈 구현

---
+++ spring20encrypt/src/main/java/com/kh/spring20/repo/SecurityRepoImpl.java
@@ -0,0 +1,40 @@
+package com.kh.spring20.repo;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.kh.spring20.dto.SecurityDto;
+
+@Service
+public class SecurityRepoImpl implements SecurityRepo {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+
+	@Override
+	  public void insert(SecurityDto securityDto) {
+	      String encrypt = encoder.encode(securityDto.getMemberPw());
+	      securityDto.setMemberPw(encrypt);
+	      
+	      sqlSession.insert("security.join", securityDto);
+	}
+
+	@Override
+	public boolean login(SecurityDto securityDto) {
+		//securityDto는 사용자가 입력한 아이디/비밀번호가 있다
+		//아이디를 이용하여 정보를 찾은 뒤 비교하도록 Encoder에게 부탁 
+		//encoder.mathches(입력값, 암호화된 값)
+		SecurityDto findDto = sqlSession.selectOne("security.find", securityDto);
+		if(encoder.matches(securityDto.getMemberPw(), findDto.getMemberPw())) {
+			return true;
+		}
+		return false;
+	}
+
+}
