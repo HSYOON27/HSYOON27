48c66ebfac3df1ece3425009666def38d187de47
HSYOON27 <kots524@naver.com>
2023-04-27 18:19:03 +0900

    웹소켓이용

---
+++ spring21websocket/src/main/java/com/kh/spring21/controller/ViewController.java
@@ -0,0 +1,28 @@
+package com.kh.spring21.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/view")
+public class ViewController {
+	
+	@GetMapping("/basic")
+	public String basic() {
+		//return "/WEB-INF/views/basic.jsp";
+		return "basic";
+	}
+	
+	@GetMapping("/chat")
+	public String chat() {
+		return "chat";
+	}
+	
+	@GetMapping("/sockjs")
+	public String sockjs() {
+		//return "/WEB-INF/views/sockjs.jsp";
+		return "sockjs";
+	}
+	
+}
