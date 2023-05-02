817c868c224849a03aa2d66062eca13a253ebccb
HSYOON27 <kots524@naver.com>
2023-05-02 15:17:06 +0900

    카카오페이 환경설정 구현

---
+++ spring22pay/src/main/java/com/kh/spring22/configuration/KakaoPayProperties.java
@@ -0,0 +1,16 @@
+package com.kh.spring22.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "custom.kakaopay")
+public class KakaoPayProperties {
+
+	private String key;
+	private String cid;
+	
+}
